45841254588cb366b967fde634c0d2ba568edc33
Mary Cusack <mary.theresa.cusack@gmail.com>
2018-06-06 16:00:57 -0400

    keys file
    keys file

---
+++ include/keys.php
@@ -0,0 +1,8 @@
+<?php
+$this->settings['oauth_access_token'] = "16055151-lTBQtBBsx7Rm4SU3L9WQjl7lpkakJfAuo7wsY2uri";
+    $this->settings['oauth_access_token_secret'] = "ZBWaQGxob42wlOIu7qT5zTkMf4fN4Sg4cWEdPlC0jsVyk";
+    $this->settings['consumer_key'] = "sxcEtBaLEaskg0xVlHUferlpn";
+    $this->settings['consumer_secret'] = "7uRJOrIQVEc8TCdtyk5Z0FH3aoXSQX1Mbz02f1bPUBViAmFAha";
+	
+	$this->apikey ='AIzaSyBDl_2-R6ldx7x8Knv5SZukhXFIdEqj_X8';
+	?>
