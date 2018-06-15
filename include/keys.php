ba752947e8891f8f3973e8b2d9b7bc6359980b5f
Mary Cusack <mary.theresa.cusack@gmail.com>
2018-06-15 09:18:15 -0400

    anonomyyisd the keys file
    replaced keys so that it won't acess my account people will have to use their own

--- include/keys.php
+++ include/keys.php
@@ -1,8 +1,8 @@
 <?php
-$this->settings['oauth_access_token'] = "16055151-lTBQtBBsx7Rm4SU3L9WQjl7lpkakJfAuo7wsY2uri";
-    $this->settings['oauth_access_token_secret'] = "ZBWaQGxob42wlOIu7qT5zTkMf4fN4Sg4cWEdPlC0jsVyk";
-    $this->settings['consumer_key'] = "sxcEtBaLEaskg0xVlHUferlpn";
-    $this->settings['consumer_secret'] = "7uRJOrIQVEc8TCdtyk5Z0FH3aoXSQX1Mbz02f1bPUBViAmFAha";
+$this->settings['oauth_access_token'] = "your oauth accsess token";
+    $this->settings['oauth_access_token_secret'] = "your oauth access token secret";
+    $this->settings['consumer_key'] = "your consumer key";
+    $this->settings['consumer_secret'] = "your consumer secret";
 	
-	$this->apikey ='AIzaSyBDl_2-R6ldx7x8Knv5SZukhXFIdEqj_X8';
+	$this->apikey ='your api key for google location';
 	?>
